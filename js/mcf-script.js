77ba9373e8203c21e214d3a031949eeb6494926b
Mirko Schubert <mirkoschubert@gmail.com>
2018-06-21 13:31:47 +0200

    gdpr consent

--- js/mcf-script.js
+++ js/mcf-script.js
@@ -4,6 +4,8 @@ jQuery(document).ready(function(e) {
     var name = e('#minimal-contact-form input.name');
     var email = e('#minimal-contact-form input.email');
     var message = e('#minimal-contact-form textarea.message');
+    var consent =
+        e('#minimal-contact-form label.consent-caption span.required');
     name.css({
       'background-color':
           (name.val() === null || name.val() === '') ? '#fdc3c4' : 'inherit'
@@ -21,6 +23,11 @@ jQuery(document).ready(function(e) {
           '#fdc3c4' :
           'inherit'
     });
+    consent.css({
+      'color': (e('#minimal-contact-form input.consent').prop('checked')) ?
+          'inherit' :
+          '#dc3232'
+    });
   });
 
   // AJAX
@@ -32,20 +39,19 @@ jQuery(document).ready(function(e) {
     data.phone = (typeof data.phone === 'undefined') ? '' : data.phone;
     data.subject = e('#minimal-contact-form input.subject').val();
     data.message = e('#minimal-contact-form textarea.message').val();
+    if (typeof e('#minimal-contact-form input.consent') !== 'undefined' &&
+        e('#minimal-contact-form input.consent').length > 0) {
+      data.consent =
+          (e('#minimal-contact-form input.consent').prop('checked')) ? 1 : 0;
+    } else
+      data.consent = 1;
 
     var is_email =
         data.email.match(/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/) !==
         null;
 
     if (data.name !== '' && data.email !== '' && is_email &&
-        data.message !== '') {
-      e('#minimal-contact-form .notice').html('');
-      e('#minimal-contact-form input.name').val('');
-      e('#minimal-contact-form input.email').val('');
-      e('#minimal-contact-form input.phone').val('');
-      e('#minimal-contact-form input.subject').val('');
-      e('#minimal-contact-form textarea.message').val('');
-
+        data.message !== '' && data.consent === 1) {
       console.log(data);
       e.ajax({
         type: 'POST',
@@ -55,6 +61,17 @@ jQuery(document).ready(function(e) {
           data: data,
         },
         success: function(t) {
+          // reset form
+          e('#minimal-contact-form input.name').val('');
+          e('#minimal-contact-form input.email').val('');
+          e('#minimal-contact-form input.phone').val('');
+          e('#minimal-contact-form input.subject').val('');
+          e('#minimal-contact-form textarea.message').val('');
+          // thank you message
+          e('#minimal-contact-form .notice')
+              .html(
+                  '<p class="success">Thank you for sending us a message! You will hear from us shortly.</p>');
+
           console.log(t);
         }
       })
--- mcf-form.php
+++ mcf-form.php
@@ -32,6 +32,23 @@ function mcf_display_contact_form() {
   $content .= ($mcf_options['spam'] === 1) ? '    <input class="phone" name="phone" placeholder="' . __('Your Phone Number', 'mcf') . '" type="text">' : '';
   $content .= '    <input class="subject" name="subject" placeholder="' . __('Subject', 'mcf') . '" type="text">';
   $content .= '    <textarea class="message" name="message" placeholder="' . __('Your Message', 'mcf') . __(' (required)', 'mcf')  . '" rows="5" required></textarea>';
+
+  if (get_option('wp_page_for_privacy_policy') || (int)get_option('wp_page_for_privacy_policy') > 0) {
+    $pp_url = get_permalink(get_option('wp_page_for_privacy_policy'));
+
+    if ($mcf_options['gdpr'] === 1) {
+      // Opt-In
+      $content .= '<input class="consent" name="consent" type="checkbox" value="1" />';
+      $content .= '<label class="consent-caption" for="consent">' . __('By sending us a message via this contact form you agree that your personal data will be processed.', 'mcf') . ' ' . __('For further information please visit our', 'mcf') . ' <a href="' . $pp_url . '" target="_blank">' . __('Privacy Policy', 'mcf') . '</a>.<span class="required">' . __(' (required)', 'mcf') . '<span></label>';
+    } else {
+      // Only Information
+      $content .= '<p class="privacy">' . __('By sending us a message via this contact form you understand that your personal data will be processed.', 'mcf') . ' ' . __('For further information please visit our', 'mcf') . ' <a href="' . $pp_url . '" target="_blank">' . __('Privacy Policy', 'mcf') . '</a>.</p>';
+
+    }
+
+  }
+
+
   $content .= '    <button class="submit" type="button">' . __('Submit', 'mcf') . '</button>';
   $content .= '  </form>';
   $content .= '</div>';
